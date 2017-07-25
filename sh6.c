18defc49c16aa51aa97eebe95b9d04f86fd9c116
Jeff Neitzel <jan@v6shell.org>
2017-07-25 23:42:53 +0000

    Update *.[1c] as needed

--- sh6.c
+++ sh6.c
@@ -73,7 +73,7 @@
 #include "sasignal.h"
 
 #if defined(CONFIG_UBUNTU_16)
-#   error "Ubuntu 16.(04|10): Not supported: see https://blog.v6shell.org/ubuntu-16/"
+#   error "Ubuntu 16.(04|10)|17.04): Not supported: see https://blog.etsh.io/ubuntu-16/"
 #endif
 
 /*
